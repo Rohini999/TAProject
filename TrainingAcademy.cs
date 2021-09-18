696be5d81364aa745d4463a851944e7cb132672d
Rohini Jadhav <rohinisj55@gmail.com>
2021-09-18 16:33:14 +0530

    Add project files.

---
+++ TrainingAcademy.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using Modell;
+using DataAccessLayerr;
+
+namespace TAProject
+{
+    public class TrainingAcademy
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("=================== ACADEMY DETAILS ========================");
+            Console.WriteLine("************************************************************");
+            DAL D = new DAL();
+
+
+            D.DisplayStudent();
+            Student s1 = D.GetInputFromUser();
+            D.AddStudent(s1);
+            D.DisplayStudent();
+            Student s2 = D.GetInputFromUser();
+            D.EditStudent(s2);
+            D.DisplayStudent();
+
+            D.DeleteStudent();
+            D.DisplayStudent();
+            D.ShowBestStudent();
+
+        }
+    }
+}
+
